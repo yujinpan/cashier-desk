d53c7ae1222203d43ff8802164c381bc556fcdd5
yujinpan <1192878390@qq.com>
2023-02-19 00:26:22 +0800

    Update deploy

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ git init
 git add .
 git commit -m 'deploy'
 
-git push -f git@github.com:yujinpan/cashier-desk.git master:gh-pages
+git push -f git@github.com:yujinpan/cashier-desk.git main:gh-pages
