60992be80d8159ae08fb3b8e835220d30fcc561d
yujinpan <1192878390@qq.com>
2023-02-20 20:52:06 +0800

    Update service worker config

--- public/sw-entry.js
+++ public/sw-entry.js
@@ -1,9 +1,7 @@
 const registerServiceWorker = async () => {
   if ('serviceWorker' in navigator) {
     try {
-      const registration = await navigator.serviceWorker.register('./sw.js', {
-        scope: '/',
-      });
+      const registration = await navigator.serviceWorker.register('./sw.js');
       if (registration.installing) {
         console.log('正在安装 Service worker');
       } else if (registration.waiting) {
