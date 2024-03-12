b45152a943d57137ca831be012ff665396f03172
yujinpan <1192878390@qq.com>
2024-03-12 14:42:55 +0800

    refactor: remove unused

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,6 @@ import Vue from 'vue';
 
 import App from './App.vue';
 import router from './router';
-import store from './store';
 import './config';
 
 // 全局组件
@@ -17,16 +16,10 @@ import './plugins';
 // 样式
 import './styles/index.scss';
 
-if (process.env.NODE_ENV === 'development') {
-  // // mock 请求配置（如果需要就去掉注释）
-  // require('./mock');
-}
-
 Vue.config.productionTip = false;
 
 new Vue({
   el: '#app',
-  store,
   router,
   render: (h) => h(App),
 });
